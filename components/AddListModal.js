ffa2dc17be872934d3d984a8c6017da117661df4
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-19 16:26:51 -0600

    Add 'close' vector icon to AddListModal component

--- components/AddListModal.js
+++ components/AddListModal.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  KeyboardAvoidingView,
+  TouchableOpacity,
+} from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "../Colors";
 
 export default class AddListModal extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Modal</Text>
-      </View>
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <TouchableOpacity style={{ position: "absolute", top: 64, right: 32 }}>
+          <AntDesign name="close" size={30} color={colors.black} />
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     );
   }
 }
