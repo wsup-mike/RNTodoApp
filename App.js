44f17750164a1322f70ee6384cf80360feb5b267
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-19 10:22:07 -0600

    Add another divider + title

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import colors from "./Colors";
 
 export default class App extends React.Component {
@@ -8,7 +8,17 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <View style={{ flexDirection: "row" }}>
-          <View style={styles.divider}></View>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo{" "}
+            <Text style={{ fontWeight: "300", color: colors.blue }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+
+      
+
+
         </View>
       </View>
     );
@@ -28,5 +38,12 @@ const styles = StyleSheet.create({
     height: 1,
     flex: 1,
     alignSelf: "center",
+  },
+
+  title: {
+    fontSize: 38,
+    fontWeight: "800",
+    color: colors.black,
+    paddingHorizontal: 64,
   },
 });
