5e506a3e5459081aa2e8457fe6bac6b5de38e616
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-19 11:56:55 -0600

    Add todo card styling

--- components/TodoList.js
+++ components/TodoList.js
@@ -1,10 +1,52 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import colors from "../Colors";
 
 export default TodoList = ({ list }) => {
   return (
-    <View>
-      <Text>{list.name}</Text>
+    <View style={[styles.listContainer, { backgroundColor: list.color }]}>
+      <Text style={styles.listTitle}>{list.name}</Text>
+
+      <View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.count}>0</Text>
+          <Text style={styles.subtitle}>Remaining</Text>
+        </View>
+        <View style={{ alignItems: "center" }}>
+          <Text style={styles.count}>0</Text>
+          <Text style={styles.subtitle}>Completed</Text>
+        </View>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  listContainer: {
+    paddingVertical: 32,
+    paddingHorizontal: 16,
+    borderRadius: 6,
+    marginHorizontal: 12,
+    alignItems: "center",
+    width: 200,
+  },
+
+  listTitle: {
+    fontSize: 24,
+    fontWeight: "700",
+    color: colors.white,
+    marginBottom: 18,
+  },
+
+  count: {
+    fontSize: 48,
+    fontWeight: "200",
+    color: colors.white,
+  },
+
+  subtitle: {
+    fontSize: 12,
+    fontWeight: "700",
+    color: colors.white,
+  },
+});
