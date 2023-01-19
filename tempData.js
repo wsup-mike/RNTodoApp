96a0c83c31c1afd4ff3a674e3204cb943d4165bb
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-19 10:47:49 -0600

    Create temporary data for todos until Firebase set

---
+++ tempData.js
@@ -0,0 +1,89 @@
+export default tempData = [
+  {
+    name: "Sams Club List",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Get gas",
+        completed: false,
+      },
+      {
+        title: "Bananas",
+        completed: false,
+      },
+      {
+        title: "Jello",
+        completed: false,
+      },
+    ],
+  },
+
+  {
+    name: "Food4Less",
+    color: "#8022D9",
+    todos: [
+      {
+        title: "Crackers",
+        completed: false,
+      },
+      {
+        title: "Energy Drink",
+        completed: false,
+      },
+      {
+        title: "Milk",
+        completed: false,
+      },
+    ],
+  },
+
+  {
+    name: "Jewel",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Quinoa",
+        completed: false,
+      },
+      {
+        title: "Flour",
+        completed: false,
+      },
+      {
+        title: "Ground Beef",
+        completed: false,
+      },
+      {
+        title: "Broccoli",
+        completed: false,
+      },
+    ],
+  },
+
+  {
+    name: "Oakton Market",
+    color: "#24A6D9",
+    todos: [
+      {
+        title: "Loganisa",
+        completed: false,
+      },
+      {
+        title: "Wrappers",
+        completed: false,
+      },
+      {
+        title: "Halibut",
+        completed: false,
+      },
+      {
+        title: "Gatorade",
+        completed: false,
+      },
+      {
+        title: "Yogurt",
+        completed: false,
+      },
+    ],
+  },
+];
