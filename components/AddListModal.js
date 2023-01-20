a704e404483c6aefad3e39230e028e309b7c1563
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-19 22:45:13 -0600

    Add 'Create' button on Modal

--- components/AddListModal.js
+++ components/AddListModal.js
@@ -5,6 +5,7 @@ import {
   StyleSheet,
   KeyboardAvoidingView,
   TouchableOpacity,
+  TextInput,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import colors from "../Colors";
@@ -19,6 +20,18 @@ export default class AddListModal extends React.Component {
         >
           <AntDesign name="close" size={30} color={colors.black} />
         </TouchableOpacity>
+
+        <View style={{ alignSelf: "stretch", marginHorizontal: 32 }}>
+          <Text style={styles.title}>Create Todo List</Text>
+          <TextInput style={styles.input} placeholder="List Name" />
+          <TouchableOpacity
+            style={[styles.create, { backgroundColor: "blue" }]}
+          >
+            <Text style={{ color: colors.white, fontWeight: "600" }}>
+              Create list
+            </Text>
+          </TouchableOpacity>
+        </View>
       </KeyboardAvoidingView>
     );
   }
@@ -29,5 +42,31 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+  },
+
+  title: {
+    fontSize: 28,
+    fontWeight: "800",
+    color: colors.black,
+    alignSelf: "center",
+    marginBottom: 16,
+  },
+
+  input: {
+    borderWidth: StyleSheet.hairlineWidth,
+    borderColor: colors.blue,
+    borderRadius: 6,
+    height: 50,
+    marginTop: 8,
+    paddingHorizontal: 16,
+    fontSize: 18,
+  },
+
+  create: {
+    marginTop: 24,
+    height: 50,
+    borderRadius: 6,
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
