3f322b1ade830bb5c0556419a1cb1d0ab8e7a214
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-20 11:07:03 -0600

    Create new class component: TodoModal.js

---
+++ components/TodoModal.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export default class TodoModal extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Screen</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
