5d81b3b6fea0a8e4f558f81de1d0cfc7affa81f4
Mike Ramos <michaelangeloram8@gmail.com>
2023-01-22 21:52:27 -0600

    Copy firebase Config data & create firebaseConfig

--- Fire.js
+++ Fire.js
@@ -1,6 +1,15 @@
 import firebase from "firebase";
 import "@firebase/firestore";
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDHVjQJnx8BHVC9nO0isGhSWRGr2bQ2GAk",
+  authDomain: "rntodoapp-db596.firebaseapp.com",
+  projectId: "rntodoapp-db596",
+  storageBucket: "rntodoapp-db596.appspot.com",
+  messagingSenderId: "869624146801",
+  appId: "1:869624146801:web:c4fa223290b71a751264d0",
+};
+
 class Fire {}
 
 export default Fire;
